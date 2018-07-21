f5b95edd771fa2dc83b88032f4540a0e3942c0c7
Joel Ballesteros <joelrb@gmail.com>
2018-07-22 01:35:49 +0800

    Complete main function to test blockchain

--- main.go
+++ main.go
@@ -4,6 +4,16 @@ import (
 	"fmt"
 )
 
-funch main() {
-	
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Test Block 1")
+	bc.AddBlock("Test Block 2")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 }
