5ba80e8d86c1bf3c7dbdefa463550c3c8be34643
Joel Ballesteros <joelrb@gmail.com>
2018-07-22 01:28:56 +0800

    The blockchain type

---
+++ blockchain.go
@@ -0,0 +1,22 @@
+package main
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+// AddBlock function to append block to blockchain
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+// NewGenesisBlock to start the blockchain
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+// NewBlockchain function to create the blockchain
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
