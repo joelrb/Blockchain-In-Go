15187156069ed7769532cf27427de3f5c07dde14
Joel Ballesteros <joelrb@gmail.com>
2018-07-22 22:01:27 +0800

    Add block serialization.

--- block.go
+++ block.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -28,4 +31,30 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 // NewGenesisBlock to start the blockchain
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+// Serialize block to boltdb
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+// DeserializeBlock to deserialize block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &block
 }
