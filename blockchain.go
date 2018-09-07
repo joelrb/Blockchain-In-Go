0e3e5c2fb1d3e0e97bc80c64fce5edcd8d72f6ea
Joel Ballesteros <joel@thinkfres.com.au>
2018-09-08 00:35:55 +0800

    Updated

--- blockchain.go
+++ blockchain.go
@@ -1,18 +1,59 @@
 package main
 
-// Blockchain type
+import (
+	"github.com/boltdb/bolt"
+)
+
+const dbFile = "blockchain.db"
+const blocksBucket = "blocks"
+
+// Blockchain sequence of blocks
 type Blockchain struct {
-	blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
-// AddBlock function to append block to blockchain
+// BlockchainIterator used to iterate over blockchain
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
+}
+
+// AddBlock saves data to blockchain
 func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
+	var lastHash []byte
+
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+
+		return nil
+	})
+
 }
 
 // NewBlockchain function to create the blockchain
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.bucket([]byte(blocksBucket))
+
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			err = b.Put([]byte("l"), genesis.Hash)
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return nil
+	})
+
+	bc := Blockchain{tip, db}
+
+	return &bc
 }
